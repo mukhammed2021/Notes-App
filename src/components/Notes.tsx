450ee1412b471b26ab71e2b99e030633ddf50102
Mukhammed <massalimov.mukhammed@mail.ru>
2024-11-14 10:26:43 +0500

    responsive

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -2,7 +2,7 @@ import Note from "./Note";
 
 export default function Notes() {
    return (
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(280px,_1fr))] gap-4">
          <Note />
          <Note />
          <Note />
