5c3464d231f35d2da4780c013b4ac5d351286761
Mukhammed <massalimov.mukhammed@mail.ru>
2024-11-16 22:05:16 +0500

    added and updated actions

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { Plus } from "lucide-react";
 import {
    Card,
    CardContent,
@@ -8,22 +10,64 @@ import {
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { type ACTIONTYPE, type Note } from "@/lib/types";
 
-export default function NoteForm() {
+interface NoteFormProps {
+   editingNote: Note | null;
+   dispatch: React.Dispatch<ACTIONTYPE>;
+}
+
+export default function NoteForm({ editingNote, dispatch }: NoteFormProps) {
+   const [note, setNote] = useState({
+      id: self.crypto.randomUUID(),
+      title: "",
+      content: "",
+   });
+
+   useEffect(() => {
+      if (editingNote) setNote(editingNote);
+   }, [editingNote]);
+
+   function handleClick() {
+      if (editingNote) {
+         dispatch({ type: "updated", payload: note });
+      } else {
+         if (note.title.trim() && note.content.trim())
+            dispatch({ type: "added", payload: note });
+      }
+      setNote({
+         id: self.crypto.randomUUID(),
+         title: "",
+         content: "",
+      });
+   }
+
    return (
       <Card className="mb-6">
          <CardHeader>
-            <CardTitle>Add New Note</CardTitle>
+            <CardTitle>{editingNote ? "Edit Note" : "Add New Note"}</CardTitle>
          </CardHeader>
          <CardContent>
-            <Input type="text" placeholder="Title" className="mb-4" />
-            <Textarea placeholder="Content" />
+            <Input
+               value={note.title}
+               onChange={(e) => setNote({ ...note, title: e.target.value })}
+               type="text"
+               placeholder="Title"
+               className="mb-4"
+            />
+            <Textarea
+               value={note.content}
+               onChange={(e) => setNote({ ...note, content: e.target.value })}
+               placeholder="Content"
+            />
          </CardContent>
          <CardFooter>
-            <Button className="inline-flex items-center gap-3">
+            <Button
+               onClick={handleClick}
+               className="inline-flex items-center gap-3"
+            >
                <Plus />
-               Add Note
+               {editingNote ? "Edit Note" : "Add Note"}
             </Button>
          </CardFooter>
       </Card>
