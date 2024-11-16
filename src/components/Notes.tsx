9985558841683413ae1f69f25a472c4789d23317
Mukhammed <massalimov.mukhammed@mail.ru>
2024-11-16 22:05:28 +0500

    notes

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,11 +1,17 @@
 import Note from "./Note";
+import { type ACTIONTYPE, type Note as NoteType } from "../lib/types";
 
-export default function Notes() {
+interface NotesProps {
+   notes: NoteType[];
+   dispatch: React.Dispatch<ACTIONTYPE>;
+}
+
+export default function Notes({ notes, dispatch }: NotesProps) {
    return (
       <div className="grid grid-cols-[repeat(auto-fit,minmax(280px,_1fr))] gap-4">
-         <Note />
-         <Note />
-         <Note />
+         {notes.map((note: NoteType) => (
+            <Note key={note.id} note={note} dispatch={dispatch} />
+         ))}
       </div>
    );
 }
