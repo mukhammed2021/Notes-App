73d51b6c7466c2b60d91c9b6a22bb983b0e99a57
Mukhammed <massalimov.mukhammed@mail.ru>
2024-11-17 20:30:08 +0500

    update provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import { NotesProvider } from "./context/NotesContext";
 
 export default function App() {
    return (
-      <NotesProvider>
-         <div className="flex min-h-svh flex-col items-center justify-center p-4">
-            <div className="mx-auto w-full max-w-4xl">
-               <h1 className="mb-6 text-3xl font-bold">Notes App</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center p-4">
+         <div className="mx-auto w-full max-w-4xl">
+            <h1 className="mb-6 text-3xl font-bold">Notes App</h1>
+            <NotesProvider>
                <NoteForm />
                <Notes />
-            </div>
+            </NotesProvider>
          </div>
-      </NotesProvider>
+      </div>
    );
 }
